195967e8528d4f7dab085a330deed21777f4eaf0
Zachary Espiritu <me@zacharyespiritu.com>
2016-01-07 11:50:17 -0500

    Added feature: Grid layout

---
+++ MathBattleII.spritebuilder/Source/Grid.swift
@@ -0,0 +1,13 @@
+//
+//  Grid.swift
+//  MathBattleII
+//
+//  Created by Zachary Espiritu on 1/7/16.
+//  Copyright © 2016 Zachary Espiritu. All rights reserved.
+//
+
+import Foundation
+
+class Grid {
+    
+}
---
+++ MathBattleII.spritebuilder/Source/Tile.swift
@@ -0,0 +1,16 @@
+//
+//  Tile.swift
+//  MathBattleII
+//
+//  Created by Zachary Espiritu on 1/7/16.
+//  Copyright © 2016 Zachary Espiritu. All rights reserved.
+//
+
+import Foundation
+
+class Tile {
+    
+    init() {
+        
+    }
+}
