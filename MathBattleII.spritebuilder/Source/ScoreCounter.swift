b2755b5cebb985ec9774eae43f34c1a879a1d268
Zachary Espiritu <me@zacharyespiritu.com>
2016-01-16 11:20:34 -0500

    Remove unnecessary ScoreCounterDelegate

--- MathBattleII.spritebuilder/Source/ScoreCounter.swift
+++ MathBattleII.spritebuilder/Source/ScoreCounter.swift
@@ -24,8 +24,6 @@ class ScoreCounter: CCSprite {
         }
     }
     
-    var delegate: ScoreCounterDelegate?
-    
     private let maximumScoreLimit = 5
     private let minimumScoreLimit = 1
     
