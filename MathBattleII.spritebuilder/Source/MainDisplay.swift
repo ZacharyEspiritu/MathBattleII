eaaa38daab95d9caa6619ccf5f2e16fcfd526649
Zachary Espiritu <me@zacharyespiritu.com>
2016-01-15 21:47:12 -0500

    Add documentation to MainDisplay.swift

--- MathBattleII.spritebuilder/Source/MainDisplay.swift
+++ MathBattleII.spritebuilder/Source/MainDisplay.swift
@@ -17,14 +17,26 @@ class MainDisplay: CCSprite {
         bottomPlayerNameLabel.string = "Testasfsdf"
     }
     
+    /**
+     Sets the `topPlayerNameLabel` string.
+     - parameter string:  the `String` to set the `topPlayerNameLabel` to
+     */
     func setTopPlayerLabel(string string: String) {
         topPlayerNameLabel.string = string
     }
     
+    /**
+     Sets the `bottomPlayerNameLabel` string.
+     - parameter string:  the `String` to set the `bottomPlayerNameLabel` to
+     */
     func setBottomPlayerLabel(string string: String) {
         bottomPlayerNameLabel.string = string
     }
     
+    /**
+     Sets the `timerLabel` string.
+     - parameter string:  the `String` to set the `timerLabel` to
+     */
     func setTimerLabel(string string: String) {
         timerLabel.string = string
     }
