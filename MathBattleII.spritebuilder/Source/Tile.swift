257fce13777dc2fd8e81c37465dc05c5a1aecb35
Zachary Espiritu <me@zacharyespiritu.com>
2016-01-15 22:14:01 -0500

    Add documentation to Tile.swift

--- MathBattleII.spritebuilder/Source/Tile.swift
+++ MathBattleII.spritebuilder/Source/Tile.swift
@@ -21,31 +21,53 @@ class Tile: CCNode {
     
     private var selected: Bool = false
     
+    /**
+     Sets the `TileValue` of the `Tile` to the supplied `value`.
+     Also updates the `TileType` of the `Tile`.
+     - parameter value:   the `TileValue` to set the `Tile` to
+     */
     func setTileValue(value: TileValue) {
         self.value = value
         type = value.checkIfNumberOrOperation()
     }
     
+    /**
+     Returns the `TileValue` of the `Tile`.
+     - returns:   the `TileValue` of the `Tile`
+     */
     func getTileValue() -> TileValue {
         return value
     }
     
+    /**
+     Returns the `TileType` of the `Tile`.
+     - returns:   the `TileType` of the `Tile`
+     */
     func getTypeType() -> TileType {
         return type
     }
     
+    /**
+     Returns a `Bool` corresponding to whether the `Tile` is selected or not.
+     - returns:   a `Bool` corresponding to whether the `Tile` is selected or not
+     */
     func isSelected() -> Bool {
         return selected
     }
     
+    /**
+     Sets `selected` to `false` and changes the appearance of the `sprite` to signal the state change.
+     */
     func selectTile() {
         selected = true
         sprite.color = CCColor(white: 0.5, alpha: 1)
     }
     
+    /**
+     Sets `selected` to `false` and restores the appearance of the `sprite` to signal the state change.
+     */
     func deselectTile() {
         selected = false
         sprite.color = CCColor(white: 1, alpha: 1)
     }
-    
 }
