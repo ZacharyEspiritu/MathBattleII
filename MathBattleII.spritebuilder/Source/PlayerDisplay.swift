61b464eb21bcdb4fa0fefc242066e851c294bad4
Zachary Espiritu <me@zacharyespiritu.com>
2016-01-15 22:35:32 -0500

    Add documentation to PlayerDisplay.swift

--- MathBattleII.spritebuilder/Source/PlayerDisplay.swift
+++ MathBattleII.spritebuilder/Source/PlayerDisplay.swift
@@ -13,18 +13,33 @@ class PlayerDisplay: CCSprite {
     weak var targetNumberLabel: CCLabelTTF!
     weak var equationLabel: CCLabelTTF!
     
+    /**
+     Sets the `targetNumberLabel` to display the `targetNumber` parameter.
+     - parameter targetNumber:   the `Int` to display
+     */
     func setTargetNumberLabel(targetNumber targetNumber: Int) {
         targetNumberLabel.string = "\(targetNumber)"
     }
     
+    /**
+     Sets the `equationLabel` to display the `equation` parameter.
+     - parameter equation:   the `String` to display
+     */
     func setEquationLabel(equation equation: String) {
         equationLabel.string = equation
     }
     
+    /**
+     Returns the `String` currently displaying on the `equationLabel`.
+     - returns:   the `String` currently displaying on the `equationLabel`
+     */
     func getEquationLabelString() -> String {
         return equationLabel.string
     }
     
+    /**
+     Clears the `equationLabel`.
+     */
     func clearEquationLabel() {
         equationLabel.string = ""
     }
