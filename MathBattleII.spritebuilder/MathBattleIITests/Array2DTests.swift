8f21796102614452b9ce7b0956587d4d1c3b6a31
Zachary Espiritu <me@zacharyespiritu.com>
2016-01-23 09:21:22 -0500

    Fix Array2D tests to ensure they work properly

--- MathBattleII.spritebuilder/MathBattleIITests/Array2DTests.swift
+++ MathBattleII.spritebuilder/MathBattleIITests/Array2DTests.swift
@@ -19,17 +19,12 @@ class Array2DTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testArraySizeInitalization() {
+        let numberOfColumns = 10
+        let numberOfRows = 15
+        let array: Array2D = Array2D<AnyObject>(columns: numberOfColumns, rows: numberOfRows)
+        XCTAssertEqual(array.columns, 10, "The number of columns in the Array2D should be \(numberOfColumns) but came out \(array.columns)")
+        XCTAssertEqual(array.rows, 15, "The number of columns in the Array2D should be \(numberOfRows) but came out \(array.rows)")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
