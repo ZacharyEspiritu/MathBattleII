a9928579c7c8e833d24de2490d14f8976346e05f
Zachary Espiritu <me@zacharyespiritu.com>
2016-01-29 13:14:07 -0500

    Split functionality between UserRegistrationScene and new RegistrationManager

--- MathBattleII.spritebuilder/Source/UserRegistrationScene.swift
+++ MathBattleII.spritebuilder/Source/UserRegistrationScene.swift
@@ -10,6 +10,8 @@ import Foundation
 
 class UserRegistrationScene: CCNode {
     
+    let manager = RegistrationManager.sharedInstance
+    
     weak var usernameTextField:        CCTextField!
     weak var emailTextField:           CCTextField!
     weak var passwordTextField:        CCTextField!
@@ -17,7 +19,7 @@ class UserRegistrationScene: CCNode {
     
     func submitRegistration() {
         do {
-            try validateRegistration()
+            try manager.validateRegistration(username: usernameTextField.string, email: emailTextField.string, password: passwordTextField.string, passwordConfirmation: confirmPasswordTextField.string)
         }
         catch RegistrationError.UsernameNotValidFormat {
             print("Username is not in a valid format.")
@@ -35,59 +37,4 @@ class UserRegistrationScene: CCNode {
             print("Something went wrong...")
         }
     }
-    
-    func validateRegistration() throws {
-        // Validate that registration fields match required format
-        guard let username = usernameTextField.string where validateUsername(username) else {
-            throw RegistrationError.UsernameNotValidFormat
-        }
-        guard let email = emailTextField.string where validateEmail(email) else {
-            throw RegistrationError.EmailNotValidFormat
-        }
-        guard let password = passwordTextField.string where validatePassword(password) else {
-            throw RegistrationError.PasswordNotValidFormat
-        }
-        guard let passwordConfirmation = confirmPasswordTextField.string where validateMatchingPasswords(password1: password, password2: passwordConfirmation) else {
-            throw RegistrationError.PasswordsDoNotMatch
-        }
-        
-        
-    }
-    
-    private func validateUsername(username: String) -> Bool {
-        if username.characters.count < 3 {
-            return false
-        }
-        return true
-    }
-    
-    private func validateEmail(email: String) -> Bool {
-        let emailRegEx = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
-        
-        guard let emailValidator: NSPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegEx) else {
-            return false
-        }
-        return emailValidator.evaluateWithObject(email)
-    }
-    
-    private func validatePassword(password: String) -> Bool {
-        if password.characters.count < 6 {
-            return false
-        }
-        return true
-    }
-    
-    private func validateMatchingPasswords(password1 password1: String, password2: String) -> Bool {
-        if password1 == password2 {
-            return true
-        }
-        return false
-    }
-}
-
-enum RegistrationError: ErrorType {
-    case UsernameNotValidFormat
-    case EmailNotValidFormat
-    case PasswordNotValidFormat
-    case PasswordsDoNotMatch
 }
