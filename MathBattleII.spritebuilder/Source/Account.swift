06b8673fee00cab0535b85dd6dc98445dd4e4ecb
Zachary Espiritu <me@zacharyespiritu.com>
2016-01-29 18:42:34 -0500

    Create Account model

---
+++ MathBattleII.spritebuilder/Source/Account.swift
@@ -0,0 +1,22 @@
+//
+//  Account.swift
+//  MathBattleII
+//
+//  Created by Zachary Espiritu on 1/29/16.
+//  Copyright © 2016 Zachary Espiritu. All rights reserved.
+//
+
+import Foundation
+
+class Account {
+    
+    var username: String
+    var email: String
+    var password: String
+    
+    init(username: String, email: String, password: String) {
+        self.username = username
+        self.email = email
+        self.password = password
+    }
+}
