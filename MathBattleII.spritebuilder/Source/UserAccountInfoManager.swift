bf67f90a7f9779e1ce4d7632cf26a9b9e066f47c
Zachary Espiritu <me@zacharyespiritu.com>
2016-02-14 18:53:06 -0500

    Initialize UserAccountInfoScene and UserAccountInfoManager

---
+++ MathBattleII.spritebuilder/Source/UserAccountInfoManager.swift
@@ -0,0 +1,16 @@
+//
+//  UserAccountInfoManager.swift
+//  MathBattleII
+//
+//  Created by Zachary Espiritu on 2/14/16.
+//  Copyright © 2016 Zachary Espiritu. All rights reserved.
+//
+
+import Foundation
+
+class UserAccountInfoManager {
+    
+    let sharedInstance = UserAccountInfoManager()
+    private init() {}
+    
+}
---
+++ MathBattleII.spritebuilder/Source/UserAccountInfoScene.swift
@@ -0,0 +1,15 @@
+//
+//  UserAccountInfoScene.swift
+//  MathBattleII
+//
+//  Created by Zachary Espiritu on 2/14/16.
+//  Copyright © 2016 Zachary Espiritu. All rights reserved.
+//
+
+import Foundation
+
+class UserAccountInfoScene: CCNode {
+    
+    let manager = UserAccountInfoManager.sharedInstance
+    
+}
