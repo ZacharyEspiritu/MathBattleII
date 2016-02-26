100c945402893d0a51d19a9e823ecf7614837181
Zachary Espiritu <me@zacharyespiritu.com>
2016-02-25 19:24:13 -0500

    Start working on Matchmaker interaction with Firebase server

--- MathBattleII.spritebuilder/Source/Matchmaker.swift
+++ MathBattleII.spritebuilder/Source/Matchmaker.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+class Matchmaker {
+    
+    func createNewUnrankedMatch() {
+        let ref = Firebase(url: Config.firebaseURL + "/matches/unranked")
+        let unrankedMatchData: [String : AnyObject?] = [
+            "hostPlayer": UserManager.sharedInstance.getCurrentUser()!.getUID(),
+            "opposingPlayer": nil
+        ]
+    }
+    
+    func createNewUnrankedMatch(withCustomName customName: String!, customPassword: String!) {
+        let ref: Firebase
+        let matchData: NSDictionary
+        if customName != nil && customPassword != nil {
+            matchData = [
+                "matchName": customName,
+                "password": customPassword,
+                "hostPlayer": UserManager.sharedInstance.getCurrentUser()!
+            ]
+            ref = Firebase(url: Config.firebaseURL + "/matches/custom")
+        }
+        else {
+            matchData = [
+                "hostPlayer": UserManager.sharedInstance.getCurrentUser()!
+            ]
+            ref = Firebase(url: Config.firebaseURL + "/matches/unranked")
+        }
+        ref.childByAppendingPath("").setValue(matchData)
+    }
+    
+    func searchForOpenMatch() {
+        
+    }
+}
