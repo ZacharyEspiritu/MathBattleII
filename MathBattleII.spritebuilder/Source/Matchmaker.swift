eedb38ed08f6fd7b3acd0f04dee41bec43702849
Zachary Espiritu <me@zacharyespiritu.com>
2016-03-06 22:31:54 -0500

    Attach to Firebase reference for matchmaking

--- MathBattleII.spritebuilder/Source/Matchmaker.swift
+++ MathBattleII.spritebuilder/Source/Matchmaker.swift
@@ -10,35 +10,95 @@ import Foundation
 
 class Matchmaker {
     
-    func createNewUnrankedMatch() {
-        let ref = Firebase(url: Config.firebaseURL + "/matches/unranked")
-        let unrankedMatchData: [String : AnyObject?] = [
-            "hostPlayer": UserManager.sharedInstance.getCurrentUser()!.getUID(),
-            "opposingPlayer": nil
+    static let sharedInstance = Matchmaker()
+    private init() {}
+    
+    var currentMatchData: MatchData?
+    
+    func createNewCustomMatch(withCustomName customName: String!, customPassword: String!) {
+        let ref = Firebase(url: Config.firebaseURL + "/matches/custom/\(customName)")
+        let hostData: NSDictionary = [
+            "uid": UserManager.sharedInstance.getCurrentUser()!.getUID(),
+            "displayName": UserManager.sharedInstance.getCurrentUser()!.getDisplayName(),
+            "isConnected": true,
+            "currentTiles": [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            "targetNumber": 0,
+            "needsToLaunch": false,
+            "score": 0
         ]
+        let matchData: NSDictionary = [
+            "password": customPassword,
+            "hostPlayer": hostData,
+            "opposingPlayer": NSNull()
+        ]
+        ref.setValue(matchData)
     }
     
-    func createNewUnrankedMatch(withCustomName customName: String!, customPassword: String!) {
-        let ref: Firebase
-        let matchData: NSDictionary
-        if customName != nil && customPassword != nil {
-            matchData = [
-                "matchName": customName,
-                "password": customPassword,
-                "hostPlayer": UserManager.sharedInstance.getCurrentUser()!
-            ]
-            ref = Firebase(url: Config.firebaseURL + "/matches/custom")
-        }
-        else {
-            matchData = [
-                "hostPlayer": UserManager.sharedInstance.getCurrentUser()!
-            ]
-            ref = Firebase(url: Config.firebaseURL + "/matches/unranked")
-        }
-        ref.childByAppendingPath("").setValue(matchData)
+    func attemptToJoinCustomMatch(matchName matchName: String, password possiblePassword: String) {
+        let ref = Firebase(url: Config.firebaseURL + "/matches/custom/" + matchName)
+        ref.observeSingleEventOfType(.Value, withBlock: { snapshot in
+            if !(snapshot.value is NSNull) { // Check if match exists
+                if snapshot.value.objectForKey("opposingPlayer") is NSNull { // Check if match is full
+                    if possiblePassword == snapshot.value.objectForKey("password") as! String { // Check if password is correct
+                        let userData: NSDictionary = [
+                            "uid": UserManager.sharedInstance.getCurrentUser()!.getUID(),
+                            "displayName": UserManager.sharedInstance.getCurrentUser()!.getDisplayName(),
+                            "isConnected": true,
+                            "currentTiles": [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            "targetNumber": 0,
+                            "needsToLaunch": false,
+                            "score": 0
+                        ]
+                        ref.childByAppendingPath("opposingPlayer").setValue(userData as [NSObject : AnyObject])
+                        
+                        let hostPlayerData = PlayerData(data: snapshot.value.objectForKey("hostPlayer") as! NSDictionary)
+                        let opposingPlayerData = PlayerData(data: userData)
+                        self.currentMatchData = MatchData(hostPlayer: hostPlayerData, opposingPlayer: opposingPlayerData)
+                        
+                        self.attachToMatchData(atRef: ref)
+                    }
+                    else {
+                        print("Incorrect password.")
+                    }
+                }
+                else {
+                    print("Match is full.")
+                }
+            }
+            else {
+                print("Match not found with name \(matchName).")
+            }
+        })
     }
     
-    func searchForOpenMatch() {
-        
+    func attachToMatchData(atRef ref: Firebase) {
+        ref.observeEventType(.Value,
+            withBlock: { snapshot in
+                if let matchData = self.currentMatchData {
+                    print("updating data")
+                    matchData.updateData(data: snapshot.value as! NSDictionary)
+                }
+            }, withCancelBlock: { error in
+                
+        })
     }
+}
+
+class MatchCreate: CCNode {
+    
+    weak var matchName: CCTextField!
+    weak var password: CCTextField!
+    
+    func create() {
+        Matchmaker.sharedInstance.createNewCustomMatch(withCustomName: matchName.string, customPassword: password.string)
+    }
+    
+    func join() {
+        Matchmaker.sharedInstance.attemptToJoinCustomMatch(matchName: matchName.string, password: password.string)
+    }
+}
+
+enum MatchmakerError: ErrorType {
+    case CustomMatchRoomDoesNotExist
+    case CustomMatchIncorrectPassword
 }
