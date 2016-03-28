192ebfc2d2d4168a1e3d0a9a28244448ae5832a8
Zachary Espiritu <me@zacharyespiritu.com>
2016-03-27 22:24:20 -0400

    Add documentation to CountdownDisplay.swift

--- MathBattleII.spritebuilder/Source/CountdownDisplay.swift
+++ MathBattleII.spritebuilder/Source/CountdownDisplay.swift
@@ -10,14 +10,24 @@ import Foundation
 
 class CountdownDisplay: CCLabelTTF {
         
+    /**
+     Displays the specified `string` on the `CountdownDisplay`.
+     - parameter string:   the `String` to be displayed on the `CountdownDisplay`
+     */
     func updateCountdownLabel(string string: String) {
         self.string = string
     }
     
+    /**
+     Hides the `CountdownDisplay`.
+     */
     func hideCountdownLabel() {
         self.visible = false
     }
     
+    /**
+     Makes the `CountdownDisplay` visible.
+     */
     func showCountdownLabel() {
         self.visible = true
     }
