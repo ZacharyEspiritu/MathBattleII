93f09f285eea4bdc1c03286f802d8974bf465bd8
Zachary Espiritu <me@zacharyespiritu.com>
2016-04-02 01:28:35 -0400

    Retrieve data from UserManager and calculate level progress for LevelDisplay

--- MathBattleII.spritebuilder/Source/LevelDisplay.swift
+++ MathBattleII.spritebuilder/Source/LevelDisplay.swift
@@ -17,15 +17,56 @@ class LevelDisplay: CCNode {
     weak var levelLabel: CCLabelTTF!
     weak var experienceLabel: CCLabelTTF!
     
+    var level: Int = 0
+    var currentProgress: Int = 0
+    var neededExperience: Int = 0
+    
     
     func didLoadFromCCB() {
+        loadData()
         animateExperienceDisplay()
+    }
+    
+    func loadData() {
+        var experienceLevel: Int = 0
+        if let user = UserManager.sharedInstance.getCurrentUser() {
+            experienceLevel = user.getExperienceLevel()
+        }
+        
+        var levelCount = 0
+        var levelExperienceCap = 10
+        while experienceLevel > 0 {
+            levelCount += 1
+            experienceLevel -= 10
+            levelExperienceCap = 10
+            if levelCount >= 2 {
+                experienceLevel -= 25 * (levelCount - 1)
+                levelExperienceCap = 25 * (levelCount - 1)
+                if levelCount >= 4 {
+                    experienceLevel -= 25 * (levelCount - 3)
+                    levelExperienceCap += 25 * (levelCount - 3)
+                    if levelCount >= 13 {
+                        experienceLevel -= 50 * (levelCount - 12)
+                        levelExperienceCap += 50 * (levelCount - 12)
+                    }
+                }
+            }
+        }
+        if experienceLevel < 0 {
+            experienceLevel = levelExperienceCap - abs(experienceLevel)
+        }
+        
+        level = levelCount
+        currentProgress = experienceLevel
+        neededExperience = levelExperienceCap
+        
+        levelLabel.string = "\(level)"
+        experienceLabel.string = "\(currentProgress)/\(neededExperience)"
     }
     
     func animateExperienceDisplay() {
         let meterMaxLength = levelMeterCenter.contentSize.width
-        let meterAnimatableLength = meterMaxLength - 10
-        let meterCalculatedLength = round(meterAnimatableLength * (135.0 / 200.0)) + 10
+        let meterCalculatedLength = round(meterMaxLength * (CGFloat(currentProgress) / CGFloat(neededExperience)))
         
         levelMeterCenter.scaleX = 0.1
         levelMeterRightEnd.position.x = levelMeterCenter.boundingBox().width
