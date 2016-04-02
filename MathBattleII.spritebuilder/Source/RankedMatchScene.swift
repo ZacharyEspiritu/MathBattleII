8a85b73026f8f6bb19073711c08c4edcec0f1a52
Zachary Espiritu <me@zacharyespiritu.com>
2016-04-02 01:27:44 -0400

    Detect touches on CoinDisplay, LevelDisplay, and RankedPlayerHeader

--- MathBattleII.spritebuilder/Source/RankedMatchScene.swift
+++ MathBattleII.spritebuilder/Source/RankedMatchScene.swift
@@ -21,6 +21,10 @@ class RankedMatchScene: CCNode {
     weak var rankedPlayerHeader: RankedPlayerHeader!
     
     
+    func didLoadFromCCB() {
+        userInteractionEnabled = true
+    }
+    
     override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
         if CGRectContainsPoint(topAreaGroupingNode.boundingBox(), touch.locationInWorld()) {
             if CGRectContainsPoint(levelDisplay.boundingBox(), touch.locationInNode(topAreaGroupingNode)) {
@@ -36,18 +40,33 @@ class RankedMatchScene: CCNode {
     }
     
     override func touchEnded(touch: CCTouch!, withEvent event: CCTouchEvent!) {
-        levelDisplay.stopAllActions()
-        levelDisplay.runAction(CCActionEaseBackOut(action: CCActionScaleTo(duration: 0.15, scale: 1)))
+        if levelDisplay.scale < 1 {
+            if CGRectContainsPoint(topAreaGroupingNode.boundingBox(), touch.locationInWorld()) {
+                if CGRectContainsPoint(levelDisplay.boundingBox(), touch.locationInNode(topAreaGroupingNode)) {
+                    print("touchLevelDisplay")
+                }
+            }
+            levelDisplay.stopAllActions()
+            levelDisplay.runAction(CCActionEaseBackOut(action: CCActionScaleTo(duration: 0.15, scale: 1)))
+        }
         
-        coinDisplay.stopAllActions()
-        coinDisplay.runAction(CCActionEaseBackOut(action: CCActionScaleTo(duration: 0.15, scale: 1)))
+        if coinDisplay.scale < 1 {
+            if CGRectContainsPoint(topAreaGroupingNode.boundingBox(), touch.locationInWorld()) {
+                if CGRectContainsPoint(coinDisplay.boundingBox(), touch.locationInNode(topAreaGroupingNode)) {
+                    print("touchCoinDisplay")
+                }
+            }
+            coinDisplay.stopAllActions()
+            coinDisplay.runAction(CCActionEaseBackOut(action: CCActionScaleTo(duration: 0.15, scale: 1)))
+        }
         
-        rankedPlayerHeader.stopAllActions()
-        rankedPlayerHeader.runAction(CCActionEaseBackOut(action: CCActionScaleTo(duration: 0.15, scale: 1)))
-    }
-    
-    func didLoadFromCCB() {
-        userInteractionEnabled = true
+        if rankedPlayerHeader.scale < 1 {
+            if CGRectContainsPoint(rankedPlayerHeader.boundingBox(), touch.locationInNode(playerHeaderGroupingNode)) {
+                segueToUserLoginScene()
+            }
+            rankedPlayerHeader.stopAllActions()
+            rankedPlayerHeader.runAction(CCActionEaseBackOut(action: CCActionScaleTo(duration: 0.15, scale: 1)))
+        }
     }
     
     func backToMenu() {
@@ -65,6 +84,18 @@ class RankedMatchScene: CCNode {
         
         let scene = CCScene()
         scene.addChild(gameplayScene)
+        
+        let transition = CCTransition(fadeWithDuration: 0.5)
+        CCDirector.sharedDirector().presentScene(scene, withTransition: transition)
+    }
+    
+    func segueToUserLoginScene() {
+        OALSimpleAudio.sharedInstance().playEffect("pop.wav")
+        
+        let userRegistrationScene = CCBReader.load("UserLoginScene") as! UserLoginScene
+        
+        let scene = CCScene()
+        scene.addChild(userRegistrationScene)
         
         let transition = CCTransition(fadeWithDuration: 0.5)
         CCDirector.sharedDirector().presentScene(scene, withTransition: transition)
