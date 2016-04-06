132bf56970f84af48404afa3da49a30dfa0732e4
Zachary Espiritu <me@zacharyespiritu.com>
2016-04-05 20:39:20 -0400

    Initialize CustomMatchScene and integrate assets

---
+++ MathBattleII.spritebuilder/Source/CustomMatchScene.swift
@@ -0,0 +1,17 @@
+//
+//  CustomMatchScene.swift
+//  MathBattleII
+//
+//  Created by Zachary Espiritu on 4/5/16.
+//  Copyright © 2016 Zachary Espiritu. All rights reserved.
+//
+
+import Foundation
+
+class CustomMatchScene: CCNode {
+    
+    weak var topAreaGroupingNode, centerAreaGroupingNode, largeButtonGroupingNode, smallButtonGroupingNode: CCNode!
+    weak var largeMenuButton, leftMenuButton, centerMenuButton, rightMenuButton: CCButton!
+    weak var newsButton, infoButton, leaderboardButton, gameCenterButton: CCButton!
+    
+}
