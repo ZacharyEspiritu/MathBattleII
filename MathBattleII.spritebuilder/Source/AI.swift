b415e11ad6ae16c49972c92d6091822569ce28ef
Zachary Espiritu <me@zacharyespiritu.com>
2016-04-21 14:25:43 -0400

    Initialize AI.swift

---
+++ MathBattleII.spritebuilder/Source/AI.swift
@@ -0,0 +1,26 @@
+//
+//  AI.swift
+//  MathBattleII
+//
+//  Created by Zachary Espiritu on 4/15/16.
+//  Copyright © 2016 Zachary Espiritu. All rights reserved.
+//
+
+import Foundation
+
+class AI {
+    var currentState: Int = 0
+    
+    func start() {
+        
+    }
+    
+    func end() {
+        
+    }
+}
+
+class BeginnerAI: AI {
+    
+    
+}
