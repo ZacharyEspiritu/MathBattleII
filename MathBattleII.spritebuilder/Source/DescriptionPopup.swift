8c1c29bda638204b818ce7197a19c3b2a399a360
Zachary Espiritu <me@zacharyespiritu.com>
2016-08-10 23:01:25 -0400

    Update DescriptionPopup messages for match menus

--- MathBattleII.spritebuilder/Source/DescriptionPopup.swift
+++ MathBattleII.spritebuilder/Source/DescriptionPopup.swift
@@ -18,9 +18,9 @@ class DescriptionPopup: CCNode {
         case .Ranked:
             setDescription(description: "Battle other players in a race\nto solve five puzzles as fast\nas possible!\n\nYou win BattlePoints by\nwinning matches, but you can\nalso lose them!")
         case .Custom:
-            setDescription(description: "Setup an online match with a\nfriend by entering the same room\nname and password!")
+            setDescription(description: "You can play online against a\nfriend by creating a Custom Match!\n\nOne player can create a match with\na custom room name and code.\n\nWhen the other player joins with the\nsame room information, an online\nmatch will begin!")
         case .Local:
-            setDescription(description: "Play on the same device\nagainst a friend!")
+            setDescription(description: "You can play on the same\ndevice against a friend in a Local\nMatch!\n\nUse the buttons in the center\nto customize your gameplay\nexperience.")
         case .Practice:
             setDescription(description: "Practice your match skills by\nattempting to solve as many\npuzzles as you can in 90\nseconds!")
         default:
